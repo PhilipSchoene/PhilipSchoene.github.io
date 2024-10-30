3781d6cc22f66236002954c442d60e07e1b6e6e0
PhilipSchoene <derschoenephilip@gmail.com>
2024-10-30 21:35:00 +0100

    test

---
+++ count.js
@@ -0,0 +1,24 @@
+const countDownDate = new Date("November 3, 2024 12:00:00").getTime();
+
+function updateCountdown() {
+    const now = new Date().getTime();
+    const distance = countDownDate - now;
+    
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+    document.getElementById("days").innerHTML = days.toString().padStart(2, '0');
+    document.getElementById("hours").innerHTML = hours.toString().padStart(2, '0');
+    document.getElementById("minutes").innerHTML = minutes.toString().padStart(2, '0');
+    document.getElementById("seconds").innerHTML = seconds.toString().padStart(2, '0');
+    
+    if (distance < 0) {
+        clearInterval(x);
+        document.querySelector(".countdown").innerHTML = "EXPIRED";
+    }
+}
+
+updateCountdown();
+const x = setInterval(updateCountdown, 1000);
